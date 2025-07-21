5a757ce52d711e749882a5a848a7eb81c78c6be9
Will Brown <wbrown_15@yahoo.com>
2025-07-21 11:02:54 -0400

    Fmt

--- cpp-lib/nix/checks.nix
+++ cpp-lib/nix/checks.nix
@@ -20,31 +20,37 @@
     mkdir -p $out
   '';
 
-  lib-name-clang-tidy = runCommand "clang-tidy" {
-    nativeBuildInputs = [
-      cmake
-      clang-tools
-      stdenv.cc
-    ];
-  } ''
-    set -e
+  lib-name-clang-tidy =
+    runCommand "clang-tidy"
+      {
+        nativeBuildInputs = [
+          cmake
+          clang-tools
+          stdenv.cc
+        ];
+      }
+      ''
+        set -e
 
-    cd ${lib-name.src}
-    ${clang-tidy-check}/bin/clang-tidy-check
-    mkdir -p $out
-  '';
+        cd ${lib-name.src}
+        ${clang-tidy-check}/bin/clang-tidy-check
+        mkdir -p $out
+      '';
 
-  lib-name-cpp-check = runCommand "cpp-check" {
-    buildInputs = [
-      cppcheck
-      libxml2
-    ];
-  } ''
-    set -e
-    WORK_DIR=$(mktemp -d)
-    cp -r ${lib-name.src}/* $WORK_DIR
-    cd $WORK_DIR
-    ${cpp-check}/bin/cpp-check
-    mkdir -p $out
-  '';
+  lib-name-cpp-check =
+    runCommand "cpp-check"
+      {
+        buildInputs = [
+          cppcheck
+          libxml2
+        ];
+      }
+      ''
+        set -e
+        WORK_DIR=$(mktemp -d)
+        cp -r ${lib-name.src}/* $WORK_DIR
+        cd $WORK_DIR
+        ${cpp-check}/bin/cpp-check
+        mkdir -p $out
+      '';
 }
